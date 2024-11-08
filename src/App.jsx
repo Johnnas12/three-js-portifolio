6a7df8414723fca8647b53d56599c04759c69e6c
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-11-08 20:01:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hero from "./sections/Hero"
+import Navbar from "./sections/navbar"
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto relative">
+           <Navbar />
+            <Hero />    
+    </main>
+  )
+}
+
+export default App
