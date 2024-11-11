5052d46b5e458c637dc584eb2a6cba651092fd8e
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-11-11 16:14:50 +0300

    bento grid done and projects section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Hero from "./sections/Hero"
 import Navbar from "./sections/navbar"
+import About from "./sections/About"
+import Projects from "./components/Projects"
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto relative">
+    <main className="max-w-7xl mx-auto">
            <Navbar />
-            <Hero />    
+            <Hero />  
+            <About />  
+            <Projects />
     </main>
   )
 }
