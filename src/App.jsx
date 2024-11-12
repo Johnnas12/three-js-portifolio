53fd6853c8e49240ea9625e16f02733f76d2420d
johnnas12 <99427393+Johnnas12@users.noreply.github.com>
2024-11-12 11:32:23 +0300

    clients section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Hero from "./sections/Hero"
 import Navbar from "./sections/navbar"
 import About from "./sections/About"
 import Projects from "./components/Projects"
+import Clients from "./sections/Clients"
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
@@ -9,6 +10,7 @@ const App = () => {
             <Hero />  
             <About />  
             <Projects />
+            <Clients />
     </main>
   )
 }
